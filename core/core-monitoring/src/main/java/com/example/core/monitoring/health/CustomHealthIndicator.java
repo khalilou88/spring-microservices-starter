befb8ca42aaca5f7ab67b830f04aaf3f5a504ce1
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-08-09 18:26:51 +0200

    wip

--- core/core-monitoring/src/main/java/com/example/core/monitoring/health/CustomHealthIndicator.java
+++ core/core-monitoring/src/main/java/com/example/core/monitoring/health/CustomHealthIndicator.java
@@ -1,7 +1,7 @@
 package com.example.core.monitoring.health;
 
-import org.springframework.boot.actuator.health.Health;
-import org.springframework.boot.actuator.health.HealthIndicator;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
 
 @Component("custom")
