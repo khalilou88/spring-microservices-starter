2f3514ed4819e7c57ccad4dc4f0b96de0589ee3a
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-08-11 20:04:15 +0200

    wip

--- services/user-service/src/test/java/com/example/userservice/controller/UserControllerTest.java
+++ services/user-service/src/test/java/com/example/userservice/controller/UserControllerTest.java
@@ -15,8 +15,8 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(UserController.class)
@@ -25,7 +25,7 @@ class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private UserService userService;
 
     @Autowired
