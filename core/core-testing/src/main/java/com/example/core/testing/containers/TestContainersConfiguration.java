2d992ed1b5dbff717945d074a2642b5d6f9cd2d3
khalilou88 <khalil.lagrida@gmail.com>
2025-08-11 18:36:07 +0200

    wip

--- core/core-testing/src/main/java/com/example/core/testing/containers/TestContainersConfiguration.java
+++ core/core-testing/src/main/java/com/example/core/testing/containers/TestContainersConfiguration.java
@@ -4,11 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.utility.DockerImageName;
 import org.testcontainers.vault.VaultContainer;
 
@@ -17,32 +17,33 @@ public class TestContainersConfiguration {
 
     private static final Logger logger = LoggerFactory.getLogger(TestContainersConfiguration.class);
 
-    @Container
-    static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>(
-                    DockerImageName.parse("postgres:15-alpine"))
-            .withDatabaseName("testdb")
-            .withUsername("test")
-            .withPassword("test")
-            .withReuse(true);
-
-    @Container
+    @Bean
     @ServiceConnection
-    static KafkaContainer kafkaContainer =
-            new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.5.0")).withReuse(true);
+    public PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:15-alpine"))
+                .withDatabaseName("testdb")
+                .withUsername("test")
+                .withPassword("test")
+                .withReuse(true);
+    }
 
-    @Container
-    static VaultContainer<?> vaultContainer = new VaultContainer<>(DockerImageName.parse("hashicorp/vault:latest"))
-            .withVaultToken("test-token")
-            .withVaultPort(8200)
-            .withSecretInVault("secret/application", "spring.datasource.password", "test")
-            .withReuse(true);
+    @Bean
+    @ServiceConnection
+    public KafkaContainer kafkaContainer() {
+        return new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.5.0")).withReuse(true);
+    }
+
+    @Bean
+    public VaultContainer<?> vaultContainer() {
+        return new VaultContainer<>(DockerImageName.parse("hashicorp/vault:latest"))
+                .withVaultToken("test-token")
+                .withVaultPort(8200)
+                .withSecretInVault("secret/application", "spring.datasource.password", "test")
+                .withReuse(true);
+    }
 
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry) {
         logger.debug(">>> Registering DynamicPropertySource properties");
-        registry.add("spring.datasource.hikari.jdbc-url", postgresContainer::getJdbcUrl);
-        registry.add("spring.datasource.hikari.username", postgresContainer::getUsername);
-        registry.add("spring.datasource.hikari.password", postgresContainer::getPassword);
-        registry.add("spring.datasource.hikari.driver-class-name", () -> "org.postgresql.Driver");
     }
 }
