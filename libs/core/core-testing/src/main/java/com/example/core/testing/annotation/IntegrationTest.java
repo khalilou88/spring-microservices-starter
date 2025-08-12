b99dc8ac712c04ffc90ddbfc21f5dc9de1c0bf46
khalilou88 <khalil.lagrida@gmail.com>
2025-08-12 19:04:36 +0200

    wip

--- libs/core/core-testing/src/main/java/com/example/core/testing/annotation/IntegrationTest.java
+++ libs/core/core-testing/src/main/java/com/example/core/testing/annotation/IntegrationTest.java
@@ -1,6 +1,7 @@
 package com.example.core.testing.annotation;
 
 import com.example.core.testing.containers.TestContainersConfiguration;
+import com.example.core.testing.listener.FlywayTestExecutionListener;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -8,10 +9,17 @@ import java.lang.annotation.Target;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Import(TestContainersConfiguration.class)
 @ActiveProfiles("test")
+@TestExecutionListeners(
+        listeners = {DependencyInjectionTestExecutionListener.class, FlywayTestExecutionListener.class},
+        mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
+@TestPropertySource(properties = "spring.flyway.clean-disabled=false")
 public @interface IntegrationTest {}
