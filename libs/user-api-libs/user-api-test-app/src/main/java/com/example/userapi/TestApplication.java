8f7c6fdcb1df07df87a739cdf296ebd29028e22f
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-08-16 14:48:06 +0200

    wip

--- libs/user-api-libs/user-api-test-app/src/main/java/com/example/userapi/TestApplication.java
+++ libs/user-api-libs/user-api-test-app/src/main/java/com/example/userapi/TestApplication.java
@@ -1,8 +1,10 @@
 package com.example.userapi;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example"})
+@EnableConfigurationProperties
 public class TestApplication {
     // This class is only used for testing
     // It provides the @SpringBootConfiguration needed by tests
