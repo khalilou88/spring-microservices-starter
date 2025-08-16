258c7acbd2bb70153d2ddbe877d9bbc10bb3873e
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-08-16 14:43:16 +0200

    wip

---
+++ libs/user-api-libs/user-api-test-app/src/main/java/com/example/userapi/TestApplication.java
@@ -0,0 +1,9 @@
+package com.example.userapi;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+    // This class is only used for testing
+    // It provides the @SpringBootConfiguration needed by tests
+}
